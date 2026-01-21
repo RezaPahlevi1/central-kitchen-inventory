0419468f7f4a8b92074e37cfd1da3805c3c01e20
RezaPahlevi1 <flashwatchreza@gmail.com>
2026-01-21 20:24:37 +0700

    feat:: add temporary sidebar

---
+++ frontend/layouts/MainLayout.jsx
@@ -0,0 +1,44 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function MainLayout() {
+  const linkClass = ({ isActive }) =>
+    `block px-4 py-2 rounded ${
+      isActive ? "bg-black text-white" : "text-gray-700 hover:bg-gray-100"
+    }`;
+
+  return (
+    <div className="flex min-h-screen">
+      {/* SIDEBAR */}
+      <aside className="w-64 border-r bg-white p-4">
+        <h1 className="text-xl font-bold mb-6">Inventory</h1>
+
+        <nav className="space-y-2">
+          <NavLink to="/dashboard" className={linkClass}>
+            Dashboard
+          </NavLink>
+
+          <NavLink to="/products" className={linkClass}>
+            Products
+          </NavLink>
+
+          <NavLink to="/products/create" className={linkClass}>
+            Create Product
+          </NavLink>
+
+          <NavLink to="/categories" className={linkClass}>
+            Categories
+          </NavLink>
+
+          <NavLink to="/stock-movements/transfer" className={linkClass}>
+            Transfer Stock
+          </NavLink>
+        </nav>
+      </aside>
+
+      {/* CONTENT */}
+      <main className="flex-1 bg-gray-50">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -5,35 +5,38 @@ import Categories from "../pages/Categories";
 import CategoryProducts from "../pages/CategoryProducts";
 import Dashboard from "../pages/Dashboard";
 import TransferStock from "../pages/TransferStock";
+import MainLayout from "../../layouts/MainLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Dashboard />,
-  },
-  {
-    path: "/products/create",
-    element: <CreateProduct />,
-  },
-  {
-    path: "/products",
-    element: <Products />,
-  },
-  {
-    path: "/categories",
-    element: <Categories />,
-  },
-  {
-    path: "/categories/:id",
-    element: <CategoryProducts />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-  },
-  {
-    path: "/stock-movements/transfer",
-    element: <TransferStock />,
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Dashboard /> },
+      {
+        path: "/products/create",
+        element: <CreateProduct />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/categories",
+        element: <Categories />,
+      },
+      {
+        path: "/categories/:id",
+        element: <CategoryProducts />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/stock-movements/transfer",
+        element: <TransferStock />,
+      },
+    ],
   },
 ]);
 
