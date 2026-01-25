1d42702fd111bfd55ee9458fc7e1941c238cd34f
RezaPahlevi1 <reza30pahlevi@gmail.com>
2026-01-26 05:40:08 +0700

    feat: commit test

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -81,7 +81,7 @@ export default function Products() {
   return (
     <div className="p-6">
       <h1 className="text-xl font-bold mb-4">Products</h1>
-
+      <span>test</span>
       <table className="w-full border">
         <thead>
           <tr className="border-b">
